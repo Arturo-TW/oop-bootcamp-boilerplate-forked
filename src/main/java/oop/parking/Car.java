d3306839861d44b205546564c77c945102e21f6a
fredrhaetw <frederico.rhae@thoughtworks.com>
2022-11-17 16:22:44 +0100

    WIP - Parking handicapped cars in accessible parkings

--- src/main/java/oop/parking/Car.java
+++ src/main/java/oop/parking/Car.java
@@ -5,37 +5,17 @@ public class Car {
     private final boolean large;
     private final boolean handicapped;
 
+    public Car(String licenseNumber, boolean large, boolean handicapped){
+        this.licenseNumber = licenseNumber;
+        this.large = large;
+        this.handicapped = handicapped;
+    }
+
     public String getLicenseNumber() {
         return licenseNumber;
     }
 
     public boolean isLarge() {
         return large;
-    }
-
-    public static class Builder {
-        private String licenseNumber;
-        private boolean large;
-        private boolean handicapped;
-
-        public Builder (String licenseNumber, boolean large) {
-            this.licenseNumber = licenseNumber;
-            this.large = large;
-        }
-
-        public Builder handicapped(boolean val) {
-            handicapped = val;
-            return this;
-        }
-
-        public Car build() {
-            return new Car(this);
-        }
-    }
-
-    private Car(Builder builder){
-        licenseNumber = builder.licenseNumber;
-        large = builder.large;
-        handicapped = builder.handicapped;
     }
 }
--- src/test/java/oop/parking/AssistantTest.java
+++ src/test/java/oop/parking/AssistantTest.java
@@ -13,7 +13,7 @@ public class AssistantTest {
 
   @BeforeMethod
   public void setUp() {
-    car = new Car.Builder("MAT-001",false).build();
+    car = new Car("MAT-001",false, false);
     parking = new Parking(2);
     assistant = new Assistant(parking);
   }
@@ -35,11 +35,11 @@ public class AssistantTest {
   public void itShouldParkCarInMultipleParkingLots() {
     Parking parking2 = new Parking(2);
     assistant = new Assistant(parking, parking2);
-    assertTrue(assistant.park(new Car.Builder("MAT-001",false).build()));
-    assertTrue(assistant.park(new Car.Builder("MAT-002",false).build()));
-    assertTrue(assistant.park(new Car.Builder("MAT-003",false).build()));
-    assertTrue(assistant.park(new Car.Builder("MAT-004",false).build()));
-    assertFalse(assistant.park(new Car.Builder("MAT-005", false).build()));
+    assertTrue(assistant.park(new Car("MAT-001",false, false)));
+    assertTrue(assistant.park(new Car("MAT-002",false, false)));
+    assertTrue(assistant.park(new Car("MAT-003",false, false)));
+    assertTrue(assistant.park(new Car("MAT-004",false, false)));
+    assertFalse(assistant.park(new Car("MAT-005", false, false)));
   }
 
   @Test
@@ -49,17 +49,17 @@ public class AssistantTest {
 
     for(int i = 1; i <= 8; i++) {
       var license = "MAT-00" + i;
-      assertTrue(assistant.park(new Car.Builder(license,false).build()));
+      assertTrue(assistant.park(new Car(license,false, false)));
     }
-    assertFalse(assistant.park(new Car.Builder("MAT-009", false).build()));
+    assertFalse(assistant.park(new Car("MAT-009", false, false)));
   }
 
 
   @Test
   public void itShouldParkLargeCars() {
     Parking parking = new Parking(2);
-    Car largeCar = new Car.Builder("MAT-001", true).build();
-    Car regularCar = new Car.Builder("MAT-002",false).build();
+    Car largeCar = new Car("MAT-001", true, false);
+    Car regularCar = new Car("MAT-002",false, false);
     Assistant assistant = new Assistant(parking);
 
     assertTrue(assistant.park(largeCar));
@@ -73,8 +73,8 @@ public class AssistantTest {
 
     assistant = new Assistant(parking, parking2);
 
-    assertTrue(assistant.park(new Car.Builder("MAT-001",true).build()));
-    assertTrue(assistant.park(new Car.Builder("MAT-002",true).build()));
+    assertTrue(assistant.park(new Car("MAT-001",true, false)));
+    assertTrue(assistant.park(new Car("MAT-002",true, false)));
 
     assertEquals(parking.availableSpace(), 4);
     assertEquals(parking2.availableSpace(), 4);
@@ -85,13 +85,11 @@ public class AssistantTest {
     Parking parking = new Parking(5);
     assistant = new Assistant(parking);
 
-    assertFalse(assistant.park(new Car.Builder("MAT-001",true)
-            .handicapped(true).build()));
+    assertFalse(assistant.park(new Car("MAT-001",true, true)));
 
     Parking parking2 = new Parking(5,true);
     assistant = new Assistant(parking2);
 
-    assertTrue(assistant.park(new Car.Builder("MAT-001",true)
-            .handicapped(true).build()));
+    assertFalse(assistant.park(new Car("MAT-001",true, true)));
   }
 }
